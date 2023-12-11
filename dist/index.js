101b643d59a73fc10998be2b3d0115537042001a
José Chavez <josmchavz@gmail.com>
2023-12-11 12:18:56 -0800

    fix: more accurate check to see if bus is on the line. pointOnLine was not accurate and gave false-positives

--- dist/index.js
+++ dist/index.js
@@ -1,93 +1,163 @@
-import { closeDb, getStops, getTrips, openDb, getVehiclePositions, getShapes } from 'gtfs';
+import { closeDb, getStops, openDb, getShapes, getStoptimes, getShapesAsGeoJSON, } from 'gtfs';
 import * as turf from '@turf/turf';
-import * as fs from "fs";
+import * as fs from 'fs';
+import geolib from 'geolib';
 let config = undefined;
 try {
-    const data = fs.readFileSync('path/to/yourfile.json', 'utf8');
+    const data = fs.readFileSync('./db/config.json', 'utf8');
     config = JSON.parse(data);
 }
 catch (error) {
     console.error('Error reading the file:', error);
 }
+function test(stopCode, busCode) {
+    let config = undefined;
+    try {
+        const data = fs.readFileSync('./db/config.json', 'utf8');
+        config = JSON.parse(data);
+    }
+    catch (error) {
+        console.error('Error reading the file:', error);
+    }
+    const db = openDb(config);
+    // Get all busses from vehicle_positions
+    const vehicles = db
+        .prepare('SELECT * ' +
+        'FROM trips ' +
+        'INNER JOIN vehicle_positions ON trips.trip_id = vehicle_positions.trip_id\n' +
+        'WHERE trips.trip_id IN (SELECT trip_id FROM vehicle_positions WHERE trip_id != \'\') ' +
+        'AND trips.route_id = ?;')
+        .all(busCode);
+    // console.log(vehicles);
+    if (vehicles.length === 0) {
+        console.log('No buses on the route');
+        closeDb(db);
+        return;
+    }
+    /*** MOVE THIS TO ITS OWN DICTIONARY LATER ***/
+    // Get the stops for the route
+    let stops = [];
+    const stopTimes = getStoptimes({
+        trip_id: vehicles[0].trip_id,
+    });
+    stopTimes.forEach((stopTime) => {
+        const [stop] = getStops({
+            stop_id: stopTime.stop_id,
+        });
+        stops.push(Object.assign(Object.assign({}, stopTime), stop));
+    });
+    // Finds the index of the current stop
+    const currentStopIndex = stops.findIndex(stop => stop.stop_code === stopCode);
+    // Gets the stops in the range of 5 stops before and the current stop
+    const rangeStops = stops
+        .slice(currentStopIndex - 5, currentStopIndex + 1)
+        .reverse();
+    /**** END HERE *****/
+    /*** MOVE THIS TO ITS OWN DICTIONARY LATER ***/
+    // Get the shape points for the route
+    const busRoutePoints = getShapes({
+        shape_id: vehicles[0].shape_id,
+    });
+    // Save as a line using turf
+    const busTurfLine = turf.lineString(busRoutePoints.map((point) => [
+        point.shape_pt_lon,
+        point.shape_pt_lat,
+    ]));
+    /******* END HERE ******/
+    const busStationLocation = turf.point([-122.02460130725074, 36.97108991791251]);
+    // Loop through the buses to know their location ETA
+    vehicles.forEach((bus) => {
+        console.log(`Bus ${bus.vehicle_id} - route ${bus.route_id} is at ${bus.latitude}, ${bus.longitude}`);
+        // Gets the location of the bus
+        const busLocation = turf.point([bus.longitude, bus.latitude]);
+        // First checks to see if the bus in at the station
+        const distance = turf.distance(busStationLocation, busLocation, { units: 'miles' });
+        const radius = 0.05;
+        if (distance <= radius) {
+            console.log("This bus is currently at or near the station. Skipping...\n");
+            return;
+        }
+        // Checks to see if the bus is in between the stops - Current and Previous
+        const currStopLocation = turf.point([
+            rangeStops[0].stop_lon,
+            rangeStops[0].stop_lat,
+        ]);
+        const prevStopLocation = turf.point([
+            rangeStops[1].stop_lon,
+            rangeStops[1].stop_lat,
+        ]);
+        const currFormerLine = turf.lineSlice(currStopLocation, prevStopLocation, busTurfLine);
+        fs.writeFileSync('./currFormerLine.json', JSON.stringify(currFormerLine));
+        const BROSFEET = 6;
+        // Convert line to polygon
+        const currFormerBuffer = turf.buffer(currFormerLine, BROSFEET, { units: 'feet' });
+        // const isComingShortly = turf.booleanPointOnLine(busLocation, currFormerLine);
+        const isComingShortly = turf.booleanPointInPolygon(busLocation, currFormerBuffer);
+        console.log('Is the bus coming shortly?', isComingShortly);
+        // Checks to see if the bus is in between the stops - Previous and 3 Stops away
+        const threeStopsAwayLocation = turf.point([
+            rangeStops[3].stop_lon,
+            rangeStops[3].stop_lat,
+        ]);
+        const prevFormerLine = turf.lineSlice(prevStopLocation, threeStopsAwayLocation, busTurfLine);
+        const prevFormerBuffer = turf.buffer(prevFormerLine, BROSFEET, { units: 'feet' });
+        fs.writeFileSync('./prevFormerLine.json', JSON.stringify(prevFormerLine));
+        // const isComingSoon = turf.booleanPointOnLine(busLocation, prevFormerLine);
+        const isComingSoon = turf.booleanPointInPolygon(busLocation, prevFormerBuffer);
+        console.log('Is the bus coming soon?', isComingSoon);
+        // Checks to see if the bus is in between the stops - 3 Stops away and 5 Stops away
+        const fiveStopsAwayLocation = turf.point([
+            rangeStops[5].stop_lon,
+            rangeStops[5].stop_lat,
+        ]);
+        const threeFormerLine = turf.lineSlice(threeStopsAwayLocation, fiveStopsAwayLocation, busTurfLine);
+        const threeFormerBuffer = turf.buffer(threeFormerLine, BROSFEET, { units: 'feet' });
+        fs.writeFileSync('./threeFormerLine.json', JSON.stringify(threeFormerLine));
+        // const isComing = turf.booleanPointOnLine(busLocation, threeFormerLine);
+        const isComing = turf.booleanPointInPolygon(busLocation, threeFormerBuffer);
+        console.log('Is the bus coming?', isComing, '\n');
+        // const toleranceDistance = 0.01
+        // // Adjusted approach for checking if the bus is coming shortly
+        // const distanceToCurrFormerLine = turf.pointToLineDistance(busLocation, currFormerLine, { units: 'miles' });
+        // console.log('distanceToCurrFormerLine', distanceToCurrFormerLine)
+        // const a = distanceToCurrFormerLine <= toleranceDistance; // toleranceDistance is the maximum distance you consider 'on the line'
+        // console.log('Is the bus coming shortly? a', a);
+        //
+        // // Adjusted approach for checking if the bus is coming soon
+        // const distanceToPrevFormerLine = turf.pointToLineDistance(busLocation, prevFormerLine, { units: 'miles' });
+        // console.log('distanceToPrevFormerLine', distanceToPrevFormerLine)
+        // const b = distanceToPrevFormerLine <= toleranceDistance;
+        // console.log('Is the bus coming soon? b', b);
+        //
+        // // Adjusted approach for checking if the bus is coming
+        // const distanceToThreeFormerLine = turf.pointToLineDistance(busLocation, threeFormerLine, { units: 'miles' });
+        // console.log('distanceToThreeFormerLine', distanceToThreeFormerLine)
+        // const c = distanceToThreeFormerLine <= toleranceDistance;
+        // console.log('Is the bus coming? c', c);
+    });
+    console.log('\nUSING GEOLIB NOW\n');
+    vehicles.forEach((bus) => {
+        console.log(`Bus ${bus.vehicle_id} - route ${bus.route_id} is at ${bus.latitude}, ${bus.longitude}`);
+        const busLocation = { latitude: bus.latitude, longitude: bus.longitude };
+        const busStationLocation = { latitude: -122.02460130725074, longitude: 36.97108991791251 };
+        const toleranceDistance = 0.01;
+        // Check if the bus is at the station
+        if (geolib.isPointWithinRadius(busLocation, busStationLocation, 321)) {
+            console.log("This bus is currently at or near the station. Skipping...");
+            return;
+        }
+        // Calculate distances to lines for 'coming shortly', 'coming soon', etc.
+        const isComingShortly = geolib.getDistanceFromLine(busLocation, { latitude: rangeStops[0].stop_lat, longitude: rangeStops[0].stop_lon }, { latitude: rangeStops[1].stop_lat, longitude: rangeStops[1].stop_lon }) <= toleranceDistance;
+        console.log('Is the bus coming shortly?', isComingShortly);
+        // Similar checks for isComingSoon and isComing with respective stop locations
+    });
+}
+test('1615', '18');
 const db = openDb(config);
-const shapePoints = getShapes({
+const shapes = getShapesAsGeoJSON({
     shape_id: 'shp-10-02',
 });
-const line = turf.lineString(shapePoints.map(point => [point.shape_pt_lon, point.shape_pt_lat]));
-// Find the location of the both stops
-const [stop1] = getStops({
-    stop_code: '1615',
-});
-const [stop2] = getStops({
-    stop_code: '1616',
-});
-// Convert to points
-const stop1Location = turf.point([stop1.stop_lon, stop1.stop_lat]);
-const stop2Location = turf.point([stop2.stop_lon, stop2.stop_lat]);
-// Cut the line into just the stops
-const sliced = turf.lineSlice(stop1Location, stop2Location, line);
-// Get the current location of the bus
-const [bus] = getVehiclePositions({
-    vehicle_id: '2226',
-});
-const busses = getVehiclePositions();
-const validBus = busses.filter(bus => {
-    const trip = getTrips({
-        trip_id: bus.trip_id,
-        route_id: '10'
-    });
-    return trip.length > 0;
-});
-console.log(validBus);
-const busLocation = turf.point([bus.longitude, bus.latitude]);
-// Checks to see if the current bus location is on the line
-const isOnLine = turf.booleanPointOnLine(busLocation, sliced, { epsilon: 0.0001 });
-console.log('Is the bus on the route?', isOnLine);
-// Check to see if bus is in between the stops
-const dummyPointInBounds = turf.point([-122.06068773618074, 37.00011662530676]);
-const dummyPointOutBounds = turf.point([-122.05679403106326, 37.000112581552976]);
-const isOnLineInBounds = turf.booleanPointOnLine(dummyPointInBounds, sliced, { epsilon: 0.0001 });
-const isOnLineOutBounds = turf.booleanPointOnLine(dummyPointOutBounds, sliced, { epsilon: 0.0001 });
-console.log('TEST: Is the bus on the route? (should be true)', isOnLineInBounds);
-console.log('TEST: Is the bus on the route? (should be false)', isOnLineOutBounds);
-// Create a point for the bus's location
-// const busLocation = turf.point([busLongitude, busLatitude]);
-// Check if the point is on the line (within a specified tolerance)
-// const isOnLine = turf.booleanPointOnLine(busLocation, line, { tolerance: 0.0001 }); // tolerance in degrees
-// console.log('Is the bus on the route?', isOnLine);
-// console.log(getShapes({
-//     shape_id: 'shp-18-02',
-// }))
-// console.log('Routes', getRoutes({
-//     route_id: '18',
-// }));
-// To get the stops for a route
-// const tripId = getTrips({
-//     route_id: '18',
-// });
-// const stopTimes = getStoptimes({
-//     trip_id: tripId[0].trip_id,
-// }, ['stop_id']);
-// stopTimes.map(stop => {
-//     const [stopName] = getStops({
-//         stop_id: stop.stop_id,
-//     });
-//     console.log(stopName.stop_name);
-// });
+// Store in a JSON file
+fs.writeFileSync('./shapes.json', JSON.stringify(shapes));
 closeDb(db);
-// const [vehicle] = getVehiclePositions({
-//     vehicle_id: '2812',
-// });
-// const stopTimes = getStopTimesUpdates({
-//     trip_id: vehicle.trip_id,
-// });
-//
-// stopTimes.map(stops => {
-//     console.log(stops);
-//     const [stop] = getStops({
-//         stop_id: stops.stop_id,
-//     });
-//     const time = new Date(stops.arrival_timestamp).toLocaleTimeString();
-//     console.log(stop.stop_name, time);
-// })
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import {
   closeDb,
   getStops,
-  getTrips,
   openDb,
-  getVehiclePositions,
   getShapes,
   getStoptimes,
   getShapesAsGeoJSON,
@@ -16,10 +14,9 @@ import {
   Stop,
   StopCombination,
   Stoptime,
-  Trip,
-  VehiclePosition,
 } from './types';
 import * as fs from 'fs';
+
 
 let config = undefined;
 
@@ -42,37 +39,29 @@ function test(stopCode: string, busCode: string) {
 
   const db = openDb(config);
 
-  // Get all the vehicle positions - RT
-  const vehicles: VehiclePosition[] =
-    getVehiclePositions() as VehiclePosition[];
-  let busArr: Bus[] = [];
+  // Get all busses from vehicle_positions
+  const vehicles: Bus[] = db
+    .prepare('SELECT * ' +
+      'FROM trips ' +
+      'INNER JOIN vehicle_positions ON trips.trip_id = vehicle_positions.trip_id\n' +
+      'WHERE trips.trip_id IN (SELECT trip_id FROM vehicle_positions WHERE trip_id != \'\') ' +
+      'AND trips.route_id = ?;')
+    .all(busCode);
 
-  // Iterate through all the vehicles and get the trip info for each
-  // Only if the trip is on the route we want
-  vehicles.forEach((bus: VehiclePosition) => {
-    const [trip]: Trip[] = getTrips({
-      trip_id: bus.trip_id,
-      route_id: busCode,
-    }) as Trip[];
+  // console.log(vehicles);
 
-    if (trip) {
-      busArr.push({
-        ...bus,
-        ...trip,
-      });
-    }
-  });
-
-  if (busArr.length === 0) {
+  if (vehicles.length === 0) {
     console.log('No buses on the route');
     closeDb(db);
     return;
   }
 
+
+  /*** MOVE THIS TO ITS OWN DICTIONARY LATER ***/
   // Get the stops for the route
   let stops: StopCombination[] = [];
   const stopTimes: Stoptime[] = getStoptimes({
-    trip_id: busArr[0].trip_id,
+    trip_id: vehicles[0].trip_id,
   }) as Stoptime[];
 
   stopTimes.forEach((stopTime: Stoptime) => {
@@ -93,10 +82,12 @@ function test(stopCode: string, busCode: string) {
   const rangeStops: StopCombination[] = stops
     .slice(currentStopIndex - 5, currentStopIndex + 1)
     .reverse();
+  /**** END HERE *****/
 
+  /*** MOVE THIS TO ITS OWN DICTIONARY LATER ***/
   // Get the shape points for the route
   const busRoutePoints: Shape[] = getShapes({
-    shape_id: busArr[0].shape_id,
+    shape_id: vehicles[0].shape_id,
   }) as Shape[];
 
   // Save as a line using turf
@@ -106,71 +97,91 @@ function test(stopCode: string, busCode: string) {
       point.shape_pt_lat,
     ])
   );
-  // Gets the location of the bus
-  const busLocation = turf.point([busArr[0].longitude, busArr[0].latitude]);
+  /******* END HERE ******/
 
-  console.log('Bus:', busArr[0]);
+  const busStationLocation = turf.point([-122.02460130725074, 36.97108991791251]);
 
-  // Checks to see if the bus is in between the stops - Current and Previous
-  const currStopLocation = turf.point([
-    rangeStops[0].stop_lon,
-    rangeStops[0].stop_lat,
-  ]);
-  const prevStopLocation = turf.point([
-    rangeStops[1].stop_lon,
-    rangeStops[1].stop_lat,
-  ]);
-  const currFormerLine = turf.lineSlice(
-    currStopLocation,
-    prevStopLocation,
-    busTurfLine
-  );
+  // Loop through the buses to know their location ETA
+  vehicles.forEach((bus: Bus) => {
+    console.log(`Bus ${bus.vehicle_id} - route ${bus.route_id} is at ${bus.latitude}, ${bus.longitude}`);
 
-  fs.writeFileSync('./currFormerLine.json', JSON.stringify(currFormerLine));
+    // Gets the location of the bus
+    const busLocation = turf.point([bus.longitude, bus.latitude]);
 
-  const isComingShortly = turf.booleanPointOnLine(busLocation, currFormerLine, {
-    epsilon: 0.0001,
+    // First checks to see if the bus in at the station
+    const distance = turf.distance(busStationLocation, busLocation, { units: 'miles' });
+    const radius = 0.05;
+
+    if (distance <= radius) {
+      console.log("This bus is currently at or near the station. Skipping...\n");
+      return;
+    }
+
+    // Checks to see if the bus is in between the stops - Current and Previous
+    const currStopLocation = turf.point([
+      rangeStops[0].stop_lon,
+      rangeStops[0].stop_lat,
+    ]);
+    const prevStopLocation = turf.point([
+      rangeStops[1].stop_lon,
+      rangeStops[1].stop_lat,
+    ]);
+    const currFormerLine = turf.lineSlice(
+      currStopLocation,
+      prevStopLocation,
+      busTurfLine
+    );
+    fs.writeFileSync('./currFormerLine.json', JSON.stringify(currFormerLine));
+
+    const expandWidth = 6;
+
+    // Convert line to polygon
+    const currFormerBuffer = turf.buffer(currFormerLine, expandWidth, { units: 'feet' });
+
+    // const isComingShortly = turf.booleanPointOnLine(busLocation, currFormerLine);
+    const isComingShortly = turf.booleanPointInPolygon(busLocation, currFormerBuffer);
+    console.log('Is the bus coming shortly?', isComingShortly);
+
+    // Checks to see if the bus is in between the stops - Previous and 3 Stops away
+    const threeStopsAwayLocation = turf.point([
+      rangeStops[3].stop_lon,
+      rangeStops[3].stop_lat,
+    ]);
+    const prevFormerLine = turf.lineSlice(
+      prevStopLocation,
+      threeStopsAwayLocation,
+      busTurfLine
+    );
+    const prevFormerBuffer = turf.buffer(prevFormerLine, expandWidth, { units: 'feet' });
+
+    fs.writeFileSync('./prevFormerLine.json', JSON.stringify(prevFormerLine));
+
+    // const isComingSoon = turf.booleanPointOnLine(busLocation, prevFormerLine);
+    const isComingSoon = turf.booleanPointInPolygon(busLocation, prevFormerBuffer);
+    console.log('Is the bus coming soon?', isComingSoon);
+
+    // Checks to see if the bus is in between the stops - 3 Stops away and 5 Stops away
+    const fiveStopsAwayLocation = turf.point([
+      rangeStops[5].stop_lon,
+      rangeStops[5].stop_lat,
+    ]);
+    const threeFormerLine = turf.lineSlice(
+      threeStopsAwayLocation,
+      fiveStopsAwayLocation,
+      busTurfLine
+    );
+    const threeFormerBuffer = turf.buffer(threeFormerLine, expandWidth, { units: 'feet' });
+
+    fs.writeFileSync('./threeFormerLine.json', JSON.stringify(threeFormerLine));
+
+    // const isComing = turf.booleanPointOnLine(busLocation, threeFormerLine);
+    const isComing = turf.booleanPointInPolygon(busLocation, threeFormerBuffer);
+    console.log('Is the bus coming?', isComing, '\n');
   });
-  console.log('Is the bus coming shortly?', isComingShortly);
 
-  // Checks to see if the bus is in between the stops - Previous and 3 Stops away
-  const threeStopsAwayLocation = turf.point([
-    rangeStops[3].stop_lon,
-    rangeStops[3].stop_lat,
-  ]);
-  const prevFormerLine = turf.lineSlice(
-    prevStopLocation,
-    threeStopsAwayLocation,
-    busTurfLine
-  );
-
-  fs.writeFileSync('./prevFormerLine.json', JSON.stringify(prevFormerLine));
-
-  const isComingSoon = turf.booleanPointOnLine(busLocation, prevFormerLine, {
-    epsilon: 0.0001,
-  });
-  console.log('Is the bus coming soon?', isComingSoon);
-
-  // Checks to see if the bus is in between the stops - 3 Stops away and 5 Stops away
-  const fiveStopsAwayLocation = turf.point([
-    rangeStops[5].stop_lon,
-    rangeStops[5].stop_lat,
-  ]);
-  const threeFormerLine = turf.lineSlice(
-    threeStopsAwayLocation,
-    fiveStopsAwayLocation,
-    busTurfLine
-  );
-
-  fs.writeFileSync('./threeFormerLine.json', JSON.stringify(threeFormerLine));
-
-  const isComing = turf.booleanPointOnLine(busLocation, threeFormerLine, {
-    epsilon: 0.0001,
-  });
-  console.log('Is the bus coming?', isComing);
 }
 
-test('1615', '20');
+test('1615', '18');
 
 const db = openDb(config);
 const shapes = getShapesAsGeoJSON({
@@ -178,43 +189,5 @@ const shapes = getShapesAsGeoJSON({
 });
 // Store in a JSON file
 fs.writeFileSync('./shapes.json', JSON.stringify(shapes));
-// const shapePoints = getShapes({
-//     shape_id: 'shp-10-02',
-// });
-// const line = turf.lineString(shapePoints.map(point => [point.shape_pt_lon, point.shape_pt_lat]));
-//
-// // Find the location of the both stops
-// const [stop1] = getStops({
-//     stop_code: '1615',
-// });
-// const [stop2] = getStops({
-//     stop_code: '1616',
-// });
-//
-// // Convert to points
-// const stop1Location = turf.point([stop1.stop_lon, stop1.stop_lat]);
-// const stop2Location = turf.point([stop2.stop_lon, stop2.stop_lat]);
-//
-// // Cut the line into just the stops
-// const sliced = turf.lineSlice(stop1Location, stop2Location, line);
-//
-// // Get the current location of the bus
-// const [bus] = getVehiclePositions({
-//     vehicle_id: '2226',
-// });
-//
-// const busLocation = turf.point([bus.longitude, bus.latitude]);
-//
-// // Checks to see if the current bus location is on the line
-// const isOnLine = turf.booleanPointOnLine(busLocation, sliced, {epsilon: 0.0001});
-// console.log('Is the bus on the route?', isOnLine);
-//
-// // Check to see if bus is in between the stops
-// const dummyPointInBounds = turf.point([-122.06068773618074, 37.00011662530676]);
-// const dummyPointOutBounds = turf.point([-122.05679403106326, 37.000112581552976]);
-// const isOnLineInBounds  = turf.booleanPointOnLine(dummyPointInBounds,  sliced, {epsilon: 0.0001});
-// const isOnLineOutBounds = turf.booleanPointOnLine(dummyPointOutBounds, sliced, {epsilon: 0.0001});
-//
-// console.log('TEST: Is the bus on the route? (should be true)', isOnLineInBounds);
-// console.log('TEST: Is the bus on the route? (should be false)', isOnLineOutBounds);
+
 closeDb(db);
